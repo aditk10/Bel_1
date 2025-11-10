524dd63470e7ebada326654c408b6b76aa885ee9
aditk10 <adityaaryanto31@guru.smk.belajar.id>
2025-11-10 14:56:43 +0700

    Update GerakanMaju.cs

--- Assets/Script/GerakanMaju.cs
+++ Assets/Script/GerakanMaju.cs
@@ -5,12 +5,24 @@ public class GerakanMaju : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World");
+        int number1 = 10;
+        int number2 = 7;
+        int number3 = number1;
+        Debug.Log("Sebelum Ditukar");
+        Debug.Log("number 1 = " +number1);
+        Debug.Log("number 2 = " + number2);
+        number1 = number2;
+        number2 = number3;
+        Debug.Log("Setelah Ditukar");
+        Debug.Log("number 1 = " +number1);
+        Debug.Log("number 2 = " + number2);
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Debug.Log("Hello World");
     }
 }
