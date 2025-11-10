0c9251da56a997b0bd8f2449a85cee4151ae72a4
aditk10 <adityaaryanto31@guru.smk.belajar.id>
2025-11-10 14:07:07 +0700

    GerakanMaju

--- Assets/Script/GerakanMaju.cs
+++ Assets/Script/GerakanMaju.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using UnityEngine.SocialPlatforms.GameCenter;
 
 public class GerakanMaju : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GameCenterPlatform
     }
 
     // Update is called once per frame
