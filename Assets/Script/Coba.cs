594ef65e82846625f9644c725e418ca60bdc449a
aditk10 <adityaaryanto31@guru.smk.belajar.id>
2025-11-11 09:16:10 +0700

    11/11/25

---
+++ Assets/Script/Coba.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Coba : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // while
+        // int nyawa = 10;
+        // while (nyawa > 0)
+        // {
+        //     nyawa = nyawa - 1;
+        //     Debug.Log(nyawa);
+        // }
+        // Debug.Log("Game Over");
+
+        //for loop
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log("iterasi ke-" +i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
